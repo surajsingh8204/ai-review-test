a17b116a1086ff8168e50d60830378b9ad56b0cd
surajsingh8204 <surajsingh8204@gmail.com>
2026-05-24 12:22:36 +0530

    added new auth processing logic

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import sqlite3
+import os
+
 
 def login(username, password):
 
-    query = f"SELECT * FROM users WHERE username='{username}'"
+    query = f"SELECT * FROM users WHERE username='{username}' AND password='{password}'"
 
     conn = sqlite3.connect("users.db")
     cursor = conn.cursor()
@@ -10,3 +12,25 @@ def login(username, password):
     cursor.execute(query)
 
     return cursor.fetchone()
+
+
+def read_file(filename):
+
+    with open(filename, "r") as f:
+        return f.read()
+
+
+def process_users(users):
+
+    results = []
+
+    for i in range(len(users)):
+
+        for j in range(len(users)):
+
+            results.append(users[i] + users[j])
+
+    return results
+
+
+API_KEY = "SUPER_SECRET_API_KEY_123"
